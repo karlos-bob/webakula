f2589f5b2fa80bea8d6f8b7aa7662e5f2bcfdb96
karlos-bob <jijalikk@gmail.com>
2019-09-18 15:09:52 +0300

    fixes

--- build/js/index.js
+++ build/js/index.js
@@ -1,86 +1,60 @@
 $(document).ready(function(){
 
-    $('.carousel').carousel({
-        touch: false
+
+    // Banner slider
+    $("#owl-carousel").owlCarousel({
+        loop:true,
+        items: 1,
+        nav: false,
+        responsive:{
+            0:{ items:1 },
+            480:{ items:1},
+            768:{ items:1},
+            1024:{ items:1}
+        },
     });
 
-    $(".carousel-indicators li").on('click', function(){
-        $(this).siblings().removeClass('active');
-        $(this).addClass('active');
-    });
-
+    // Popover
     $('[data-toggle="popover"]').popover();
 
-
-    $('.tab-inner-1').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
+    // Tab slider-1
+    $('.tab-inner-1').owlCarousel({
+        loop: false,
+        items: 3,
+        nav: true,
         dots: false,
-        speed: 1000,
-        arrows: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-
+        margin: 30,
+        responsive:{
+            0:{ items:1 },
+            480:{ items:1},
+            768:{ items:2},
+            970:{ items:2},
+            992:{ items:2},
+            1024:{ items:2},
+            1200:{ items:3}
+        },
     });
 
-    $('.tab-inner-2').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
+    // Tab slider-2
+    $('.tab-inner-2').owlCarousel({
+        loop: false,
+        items: 3,
+        nav: true,
         dots: false,
-        speed: 1000,
-        arrows: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
+        margin: 30,
+        responsive:{
+            0:{ items:1 },
+            480:{ items:1},
+            768:{ items:1},
+            970:{ items:2},
+            992:{ items:2},
+            1024:{ items:2},
+            1200:{ items:3}
+        },
     });
 
-    $('#offers-tab a').on('click', function (e) {
-        e.preventDefault();
-        $(this).tab('show');
-    });
+    // $('#offers-tab a').on('click', function (e) {
+    //     e.preventDefault();
+    //     $(this).tab('show');
+    // });
 });
